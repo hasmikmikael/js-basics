dfeb743a5f151620748dd8ca2ef200ac6c9589cb
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-14 03:44:35 +0400

    Variables.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-// Comment
-console.log('Hello World');
+let name = 'Tim';
+console.log(name);
+
+// Names of variables
+// Cannot be a reserved keywords: if, else, let, var etc.
+// Should be meaningful: a1, x etc.
+// Cannot start with a number: 1name
+// Cannot contain a space or hyphen: -
+// Are case-sensitive
+
+let firstName = 'Tim'; //Camel Notation
+let lastName = 'Smith';
