a87469978101655fe53c527f98c304d7c1fc2cd0
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-14 20:31:37 +0400

    Types of Functions.

--- index.js
+++ index.js
@@ -49,8 +49,16 @@ console.log(selectedColors);
 console.log(selectedColors.length);
 
 //---------Functions---------------
+// Performing a task
 function greet(name, lastName) {
     console.log('Hello ' + name + ' ' + lastName);
 }
 
 greet('John', 'Smith');
+
+// Calculating a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
