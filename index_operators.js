87e779b223c32ed14610b56034d0c21dba306272
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-15 00:39:57 +0400

    Arithmetic Operators.

---
+++ index_operators.js
@@ -0,0 +1,22 @@
+//-----------Arithmetic Operators--------------
+let x = 10;
+let y = 3;
+let z = 5;
+let k = 7;
+
+console.log(x + y); // Addition Operator
+console.log(x - y); // Subtraction Operator
+console.log(x * y); // Multiplication Operator
+console.log(x / y); // Division Operator
+console.log(x % y); // Remainder of Division
+console.log(x ** y); // Exponentiation Operator (x to the power of y)
+
+// Increment Operator (++)
+console.log(++x); // 11
+console.log(y++); // 3
+console.log(y); // 4
+
+// Decrement Operator (--)
+console.log(z--); // 5
+console.log(z); // 4
+console.log(--k); // 6
