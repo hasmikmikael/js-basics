2c4e2f27a870ba668b0c84e37408eb5cb4d421d7
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-17 02:44:55 +0400

    While

--- index_control_flow.js
+++ index_control_flow.js
@@ -65,3 +65,10 @@ for (let i = 1; i <= 5; i++) {
 for (let i = 5; i >= 1; i--) {
     if (i % 2 !== 0) console.log(i);
 }
+
+//-----------While------------------
+let i = 0;
+while (i <= 5) {
+    if (i % 2 !== 0) console.log(i);
+    i++;
+}
