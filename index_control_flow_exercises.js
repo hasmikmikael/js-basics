5fb18c1a521e856c5e3694eba8ee68f8cdd440a3
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-18 02:20:04 +0400

    Count Truthy.

--- index_control_flow_exercises.js
+++ index_control_flow_exercises.js
@@ -84,3 +84,17 @@ function showNumbers(limit) {
       console.log(i, message);
     }
 }
+
+// exercise 6: Count Truthy
+
+const array = [null, undefined, 0, true, 1, 2, 3];
+
+console.log(countTruthy(array));
+
+function countTruthy(array) {
+    let count = 0;
+    for (let value of array)
+      if (value)
+        count++;
+    return count;
+}
