99641f57b631dd3f1a1ae645f9ad0e9ce39326d7
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-17 23:35:00 +0400

    Exersice-Landscape or Portrait.

--- index_control_flow_exercises.js
+++ index_control_flow_exercises.js
@@ -10,3 +10,13 @@ function max(number1, number2) {
 
     return number1 > number2 ? number1 : number2;
 }
+
+// Exercise 2: Write a function that takes 2 parameters
+// width and height of an image and returns true if the image
+// is landscape (width > height) otherwise it returs false.
+
+console.log(isLandscape(800, 600));
+
+function isLandscape(width, height) {
+    return (width > height);
+}
