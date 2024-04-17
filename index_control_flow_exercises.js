089fd06136635efa2f0da3d31e29b2c31cab3fd4
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-18 00:01:38 +0400

    FizzBuzz.

--- index_control_flow_exercises.js
+++ index_control_flow_exercises.js
@@ -20,3 +20,30 @@ console.log(isLandscape(800, 600));
 function isLandscape(width, height) {
     return (width > height);
 }
+
+// Exercise 3: FizzBuzz
+
+// Divisible by 3 => Fizz
+// Divisible by 5 => Buzz
+// Divisible by both 3 and 5 => FizzBuzz
+// Not divisible by 3 or 5 => input
+// Not a number => 'Not a number'
+
+const output = fizzBuzz(7);
+console.log(output);
+
+function fizzBuzz(input) {
+    if (typeof input !== 'number')
+      return NaN;
+    
+    if ((input % 3 === 0) && (input % 5 === 0)) 
+      return 'FizzBuzz';
+
+    if (input % 3 === 0) 
+      return 'Fizz';
+
+    if (input % 5 === 0) 
+      return 'Buzz';  
+    
+    return input;
+}
