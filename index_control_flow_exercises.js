974f5ddd4b87c13e2d3b9a8bd973a5e79a6d32ed
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-18 00:52:42 +0400

    Demerit Points.

--- index_control_flow_exercises.js
+++ index_control_flow_exercises.js
@@ -44,6 +44,32 @@ function fizzBuzz(input) {
 
     if (input % 5 === 0) 
       return 'Buzz';  
-    
+
     return input;
+}
+
+
+// Exercise 4: Demerit Points
+
+// Speed Limit = 70
+// 5 -> 1 point
+// Math.floor(1.3)
+// 12 points -> suspended
+
+checkSpeed(70);
+
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint) {
+      console.log('Ok');
+      return;
+    }
+
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12)
+      console.log('License suspended');
+    else
+      console.log('Point: ' + points);
 }
