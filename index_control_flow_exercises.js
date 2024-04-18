c799b911aee2a48158280b0fc810c28e7a38ff61
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-18 05:54:32 +0400

    Exercise-Prime Numbers.

--- index_control_flow_exercises.js
+++ index_control_flow_exercises.js
@@ -175,3 +175,40 @@ function showStars(rows) {
       console.log(pattern);
     }
 }
+
+// Exercise 11: Prime Numbers
+
+showPrimes(10);
+
+// Prime (whose factors are only 1 and itself)
+// Composite
+
+// 12 = 1, 2, 3, 4, 6, 12 // 12 is composite number
+// Can be divided evenly by its factors
+
+// 11 = 1, 11
+// 13 = 1, 13
+
+function showPrimes(limit) {
+    for (let number = 2; number <= limit; number++) {
+
+    //   let isPrime = true;
+    //   for (let factor = 2; factor < number; factor++) {
+    //     if (number % factor === 0) {
+    //       isPrime = false;
+    //       break;
+    //     }
+    //   }
+
+    //   if (isPrime) console.log(number);
+
+      if (isPrime(number)) console.log(number);     
+    }
+}
+
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0)
+      return false;
+  return true;
+}
