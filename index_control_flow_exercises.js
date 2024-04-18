a26ee5755e4cf4263aa9ed56b6ece34bb99c8bf8
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-18 04:55:32 +0400

    Exercise-Sum of Multiples of 3 and 5.

--- index_control_flow_exercises.js
+++ index_control_flow_exercises.js
@@ -115,3 +115,20 @@ function showProperties(obj) {
       if (typeof obj[key] === 'string')
         console.log(key, obj[key]);
 }
+
+// Exercise 8: Sum of Multiples of 3 and 5
+
+console.log(sum(10));
+
+// Multiples of 3: 3, 6, 9
+// Multiples of 5: 5, 10
+
+function sum(limit) {
+    let sum = 0;
+    for (let i = 0; i <= limit; i++)
+      if (i % 3 === 0 || i % 5 === 0) {
+        console.log(i);
+        sum += i;
+      }
+    return sum;
+}
