0e0d39f00949b8bebea322b72d9dc3185e4f6a71
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-18 05:26:40 +0400

    Exercise-Stars.

--- index_control_flow_exercises.js
+++ index_control_flow_exercises.js
@@ -162,3 +162,16 @@ function calculateAverage(array) {
     sum += mark;
   return sum / array.length;
 }
+
+// Exercise 10: Stars
+
+showStars(10);
+
+function showStars(rows) {
+    for (let i = 0; i < rows; i++) {
+      let pattern = '';
+      for (let j = 0; j < i + 1; j++)
+        pattern += '*';
+      console.log(pattern);
+    }
+}
