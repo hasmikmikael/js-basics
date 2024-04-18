44ce835e6abd86829bcf7341bcb7e37c30dd9c98
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-18 05:14:25 +0400

    Exercise-Grade.

--- index_control_flow_exercises.js
+++ index_control_flow_exercises.js
@@ -132,3 +132,33 @@ function sum(limit) {
       }
     return sum;
 }
+
+// Exercise 9: Grade
+
+const marks = [80, 80, 50];
+
+// Average = 80 + 80 + 50 / 3 = 70
+
+// 0-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+console.log(calculateGrade(marks));
+
+function calculateGrade(marks) {
+  const average = calculateAverage(marks);
+  if (average < 60) return 'F';
+  if (average < 70) return 'D';
+  if (average < 80) return 'C';
+  if (average < 90) return 'B';
+  return 'A';
+}
+
+function calculateAverage(array) {
+  let sum = 0;
+  for (let mark of array)
+    sum += mark;
+  return sum / array.length;
+}
