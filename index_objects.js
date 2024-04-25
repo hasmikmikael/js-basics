b3458630ada96230cb79eb83d503fe7a01d2f410
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-26 03:36:22 +0400

    Objects-Basics.

---
+++ index_objects.js
@@ -0,0 +1,17 @@
+//---------Basics----------
+
+// Object-oriented Programming (OOP)
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+circle.draw(); // Method
