65ef9376a6d3ccf998acb66dad3621db23b459e0
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-26 03:48:16 +0400

    Factory Functions.

--- index_objects.js
+++ index_objects.js
@@ -15,3 +15,21 @@ const circle = {
 };
 
 circle.draw(); // Method
+
+
+//----------Factory Functions-----------
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw() {
+            console.log('draw');
+        }
+    };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
