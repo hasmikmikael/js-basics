8a1172a78722fc401fb3ee367f3ac24de34caa32
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-05-06 03:23:16 +0400

    Exercise - Factory and Constructor Functions.

--- index_objects_exercises.js
+++ index_objects_exercises.js
@@ -11,3 +11,26 @@ function showAddress(address) {
 }
 
 showAddress(address);
+
+// Exercise 2: Factory and Constructor Functions
+// Factory Function
+function createAddress(street, city, zipCode) {
+    return {
+        street,
+        city,
+        zipCode
+    };
+}
+
+const addr = createAddress('5', 'New York', '2213');
+console.log(addr);
+
+// Constructor Function
+function Address(street, city, zipCode) {
+    this.street = street,
+    this.city = city,
+    this.zipCode = zipCode
+}
+
+const add = new Address('5', 'New York', '2213');
+console.log(add);
