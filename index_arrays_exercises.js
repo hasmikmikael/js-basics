badab6452adc2dd3e551424f79275aa363f7bba2
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-05-08 02:44:37 +0400

    Exercise 2 - Includes.

--- index_arrays_exercises.js
+++ index_arrays_exercises.js
@@ -9,3 +9,15 @@ function arrayFromRange(min, max) {
     output.push(i);
    return output;
 }
+
+// Exercise 2: Includes
+const nmbrs = [1, 2, 3, 4];
+
+console.log(includes(nmbrs, 3));
+
+function includes(array, searchElement) {
+  for (let element of array)
+    if (element === searchElement)
+      return true;
+  return false;
+}
