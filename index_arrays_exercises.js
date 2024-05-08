27e05d78b21b20c5171b16bb96c481d21e047e8c
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-05-09 00:51:53 +0400

    Exercise - Moving an Element.

--- index_arrays_exercises.js
+++ index_arrays_exercises.js
@@ -36,3 +36,22 @@ function except(array, excluded) {
       output.push(element);  
   return output;
 }
+
+// Exercise 4: Moving an Element
+const numbersM = [1, 2, 3, 4];
+
+const outputM = move(numbersM, 1, 1);
+
+console.log(outputM);
+
+function move(array, index, offset) {
+    const position = index + offset;
+    if (position >= array.length || position < 0) {
+      console.log('Invalid offset.');
+      return;
+    }
+    const output = [...array];
+    const element = output.splice(index, 1)[0];
+    output.splice(position, 0, element);
+    return output;
+}
