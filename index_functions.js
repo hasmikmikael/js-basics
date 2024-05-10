4d35312ea0475de8a68624dad8bf8b1ac985bf93
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-05-11 03:18:26 +0400

    Default Parameters.

--- index_functions.js
+++ index_functions.js
@@ -68,3 +68,42 @@ function sumArg() {
 }
 
 console.log(sumArg(1, 2, 3, 4, 5)); //15
+
+
+//----------Default Parameters--------------
+// function interest(principal, rate, years) {
+//     return principal * rate / 100 * years;
+// }
+// console.log(interest(10000, 3.5, 5)); //1750
+
+
+// function interest(principal, rate, years) {
+//     rate = rate || 3.5;
+//     years = years || 5;
+
+//     return principal * rate / 100 * years;
+// }
+// console.log(interest(10000)); //1750
+
+
+// // from the ES6 we have a cleaner way
+// function interest(principal, rate = 3.5, years = 5) {
+//     return principal * rate / 100 * years;
+// }
+// console.log(interest(10000));
+
+
+// function interest(principal, rate = 3.5, years) {
+//     return principal * rate / 100 * years;
+// }
+// console.log(interest(10000)); // NaN
+// console.log(interest(10000, 5)); // NaN
+// // this one works but the code is ugly
+// console.log(interest(10000, undefined, 5)); // 1750
+
+// best practice
+function interest(principal, years, rate = 3.5) {
+    return principal * rate / 100 * years;
+}
+
+console.log(interest(10000, 5));
