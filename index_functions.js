90cea99aaa4611532fad8d45b193bc14cfec91c4
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-05-11 01:41:44 +0400

    Hoisting.

--- index_functions.js
+++ index_functions.js
@@ -18,3 +18,33 @@ const run = function() {
 let move = run;
 run(); // run
 move(); // run
+
+
+//----------Hoisting-------------
+// Hoisting is a process of moving 
+// function declaration to the top of the file.
+
+// we can call a function that is defined using
+// the function declaration syntax before it's definition.
+
+// Function Declaration
+walk();
+
+function walk() {
+    console.log('walk');
+}
+
+// // Function Expression
+// // we can't write like this we will get an error
+// run();
+
+// const run = function() {
+//     console.log('run');
+// };
+
+// // we should write like this
+// const run = function() {
+//     console.log('run');
+// };
+
+// run();
