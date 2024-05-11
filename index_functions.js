d09631754ffb6a53bbe32d481e8cc954a98a41be
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-05-11 22:09:43 +0400

    Try and Catch.

--- index_functions.js
+++ index_functions.js
@@ -136,3 +136,29 @@ const person = {
 
 person.fullName = 'John Smith';
 console.log(person); // {firstName: 'John', lastName: 'Smith'}
+
+
+//--------Try and Catch----------------
+const personE = {
+    firstName: 'Tim',
+    lastName: 'Smith',
+    set fullName(value) {
+      if (typeof value !== 'string')
+        throw new Error('Value is not a string.');
+    
+      const parts = value.split(' ');
+      if (parts.length !== 2)
+        throw new Error('Enter a first and last name.');
+
+      this.firstName = parts[0];
+      this.lastName = parts[1];
+    }
+};
+
+try {
+    // personE.fullName = null; // Value is not a string.
+    personE.fullName = ''; // Enter a first and last name.
+}
+catch(e) {
+    alert(e);
+}
