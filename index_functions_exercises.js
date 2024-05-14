621242bc7a4afeaff868cdf057d7813e7efea03e
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-05-14 22:34:34 +0400

    Exercise - Area of Circle.

--- index_functions_exercises.js
+++ index_functions_exercises.js
@@ -20,3 +20,15 @@ function sum(...items) {
 
     return items.reduce((a, b) => a + b);
 }
+
+
+// Exercise 2: Area of Circle
+
+const circle = {
+    radius: 1,
+    get area() {
+      return Math.PI * this.radius * this.radius;
+    }
+};
+
+console.log(circle.area);
